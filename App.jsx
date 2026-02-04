04a4ec7bf9693bb613f3a10c9353274cd861b94f
Meha <mehavenkat.outlook@gmail.com>
2026-02-04 20:32:49 +0530

    Add App.jsx landing page

---
+++ App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="app-container">
+      <div>
+        <h1>Paradise Nursery</h1>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
